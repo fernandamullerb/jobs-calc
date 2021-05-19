7455f73c4f60aad53cfe1c56b49d3c8013e0c5d5
Fernanda Müller Baldi <fernandamullerb@gmail.com>
2021-05-19 17:45:24 -0300

    Header refactoring

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,18 @@ const routes = express.Router()
 
 const views = __dirname + "/views/"
 
+const profile = {
+    name: "Fernanda",
+    avatar: "https://avatars.githubusercontent.com/u/76785354?v=4",
+    "monthly-budget": 3000,
+    "hours-per-day": 5,
+    "days-per-week": 5,
+    "vacation-per-year": 4
+}
+
 routes.get('/', (req, res) => res.render(views + "index"))
 routes.get('/job', (req, res) => res.render(views + "job"))
 routes.get('/job-edit', (req, res) => res.render(views + "job-edit"))
-routes.get('/profile', (req, res) => res.render(views + "profile"))
+routes.get('/profile', (req, res) => res.render(views + "profile", {profile}))
 
 module.exports = routes
