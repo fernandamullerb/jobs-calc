2cebcd455581b1e9a62afc0f7ac783eb5313a5d6
Fernanda Müller Baldi <fernandamullerb@gmail.com>
2021-05-25 13:18:19 -0300

    Update server.js
    Code refactoring

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 const express = require("express")
 const routes = require("./routes")
 const server = express()
+const path = require("path")
 
 server.set('view engine', 'ejs')
 
 //habilitar arquivos estáticos:
 server.use(express.static("public"))
+
+//mudar a localização da pasta views:
+server.set('views', path.join(__dirname, "/views/"))
 
 //usar o req.body:
 server.use(express.urlencoded())
