636be304ef34e5cfa86585d0bfdf764be198ee5f
Fernanda Müller Baldi <fernandamullerb@gmail.com>
2021-05-26 13:15:04 -0300

    Update jobUtils.js

--- src/utils/jobUtils.js
+++ src/utils/jobUtils.js
@@ -1,7 +1,7 @@
 module.exports = {
     remainingDays(job) {
         const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed()
-    
+
         const createdDate = new Date(job.created_at)
 
         const dueDay = createdDate.getDate() + Number(remainingDays)
