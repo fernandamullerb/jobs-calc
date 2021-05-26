19e4a015aa9f7fedf543b53f51f37d3f38e65016
Fernanda Müller Baldi <fernandamullerb@gmail.com>
2021-05-26 13:14:45 -0300

    Update ProfileController.js

--- src/controllers/ProfileController.js
+++ src/controllers/ProfileController.js
@@ -3,15 +3,16 @@ const Profile = require('../model/Profile')
 module.exports = {
     index(req, res) {
         return res.render("profile", { profile: Profile.get() }
-    )},
+        )
+    },
     update(req, res) {
-       const data = req.body
+        const data = req.body
 
-       const weeksPerYear = 52
+        const weeksPerYear = 52
 
-       const weeksPerMonth = (weeksPerYear - data["vacation-per-year"]) / 12
+        const weeksPerMonth = (weeksPerYear - data["vacation-per-year"]) / 12
 
-       const weekTotalHours = data["hours-per-day"] * data["days-per-week"]
+        const weekTotalHours = data["hours-per-day"] * data["days-per-week"]
 
         const monthlyTotalHours = weekTotalHours * weeksPerMonth
 
