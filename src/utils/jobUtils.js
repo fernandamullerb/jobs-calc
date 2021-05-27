443cef3dafe7c49974e2e6fb2dfc85144d300711
Fernanda Müller Baldi <fernandamullerb@gmail.com>
2021-05-27 12:14:08 -0300

    Update jobUtils.js

--- src/utils/jobUtils.js
+++ src/utils/jobUtils.js
@@ -13,7 +13,7 @@ module.exports = {
         //transformar milissegundos em dias:
         const dayInMs = 1000 * 60 * 60 * 24
 
-        const dayDiff = Math.floor(timeDiffInMs / dayInMs)
+        const dayDiff = Math.ceil(timeDiffInMs / dayInMs)
 
         return dayDiff
     },
