eb80b35223b90cdc3590d11b7b37f529db5626ee
Fernanda Müller Baldi <fernandamullerb@gmail.com>
2021-05-27 12:13:52 -0300

    Update ProfileController.js

--- src/controllers/ProfileController.js
+++ src/controllers/ProfileController.js
@@ -1,11 +1,11 @@
 const Profile = require('../model/Profile')
 
 module.exports = {
-    index(req, res) {
-        return res.render("profile", { profile: Profile.get() }
+    async index(req, res) {
+        return res.render("profile", { profile: await Profile.get() }
         )
     },
-    update(req, res) {
+    async update(req, res) {
         const data = req.body
 
         const weeksPerYear = 52
@@ -19,8 +19,10 @@ module.exports = {
         //valor da minha hora:
         const valueHour = data["monthly-budget"] / monthlyTotalHours
 
-        Profile.update({
-            ...Profile.get(),
+        const profile = await Profile.get() 
+
+        await Profile.update({
+            ...profile,
             ...req.body,
             "value-hour": valueHour
         })
